7be7be624acd592247d948a251566d68a3d5306d
rlacjs1021@naver.com <rlacjs1021@naver.com>
2022-03-19 16:44:16 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        bottom: PreferredSize(
+          preferredSize: Size(0, 60), // bottom 영역 크기
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
